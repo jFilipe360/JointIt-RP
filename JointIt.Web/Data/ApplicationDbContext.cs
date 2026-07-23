2005ed9fcd82d6f491de722e3eb21b3058e097c4
jfgc <jf.gc@outlook.pt>
2026-07-23 18:00:40 +0100

    Adicionar arquivos de projeto.

---
+++ JointIt.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JointIt.Web.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
