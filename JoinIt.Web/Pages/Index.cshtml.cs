90bd4a6448653543ba6b3523d293071ad663ad7b
jfgc <jf.gc@outlook.pt>
2026-09-10 15:40:45 +0100

    Atualização da interface gráfica da componente web

--- JoinIt.Web/Pages/Index.cshtml.cs
+++ JoinIt.Web/Pages/Index.cshtml.cs
@@ -1,13 +1,44 @@
+using JoinIt.Web.Data;
+using JoinIt.Web.Enums;
+using JoinIt.Web.Models;
+using JoinIt.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace JoinIt.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _context;
+        private readonly IEstadoEventoService _estadoEventoService;
 
+        public IndexModel(
+            ApplicationDbContext context,
+            IEstadoEventoService estadoEventoService)
+        {
+            _context = context;
+            _estadoEventoService = estadoEventoService;
+        }
+        public IList<Evento> ProximosEventos { get; private set; } = new List<Evento>();
+
+
+        public async Task OnGetAsync()
+        {
+            await _estadoEventoService.AtualizarEstadosAsync();
+
+            ProximosEventos = await _context.Eventos
+                .AsNoTracking()
+                .Where(e =>
+                    !e.IsPrivado &&
+                    e.Estado == EstadoEvento.ParaBreve &&
+                    e.DataHora > DateTime.Now)
+                .Include(e => e.EventosCategorias)
+                    .ThenInclude(ec => ec.Categoria)
+                .Include(e => e.Participantes)
+                .OrderBy(e => e.DataHora)
+                .Take(6)
+                .ToListAsync();
         }
     }
 }
